26ef31266a8fe354b86cf27ba8e4d08bf0dd6227
Florian Berger <florian.berger@efrei.net>
2025-09-25 10:06:57 +0200

    Creation labyrinthe

--- Labyrinth/Program.cs
+++ Labyrinth/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+const string lab = """
+                   +--+--------+
+                   |  /        |
+                   |  +--+--+  |
+                   |     |k    |
+                   +--+  |  +--+
+                      |k       |
+                   +  +-------/|
+                   |           |
+                   +-----------+
+                   """;
+
+Console.WriteLine("Initialisation du labyrinthe :");
+Labyrinth.Labyrinth labyrinth = new Labyrinth.Labyrinth(lab);
